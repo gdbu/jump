81206a739b0ac4841b979526161f6574295faac7
Josh Montoya <josh@montoya.io>
2019-02-20 17:11:16 -0800

    Move CreateUser method to user.go

--- jump.go
+++ jump.go
@@ -139,32 +139,6 @@ func (j *Jump) SetPermission(resourceName, resourceID, group string, actions, ad
 	return j.setPermission(resourceKey, group, actions, adminActions)
 }
 
-// CreateUser will create a user and assign it's basic groups
-// Note: It is advised that this function is used when creating users rather than directly calling j.Users().New()
-func (j *Jump) CreateUser(email, password string, groups ...string) (userID, apiKey string, err error) {
-	if userID, err = j.usrs.New(email, password); err != nil {
-		return
-	}
-
-	// Ensure first group is the user group
-	groups = append([]string{userID}, groups...)
-
-	// Add groups to user
-	if err = j.perm.AddGroup(userID, groups...); err != nil {
-		return
-	}
-
-	if apiKey, err = j.api.New(userID, "primary"); err != nil {
-		return
-	}
-
-	if err = j.SetPermission("user", userID, userID, permRWD, permRWD); err != nil {
-		return
-	}
-
-	return
-}
-
 // Users will return the users controller
 func (j *Jump) Users() *users.Users {
 	return j.usrs
---
+++ user.go
@@ -0,0 +1,27 @@
+package jump
+
+// CreateUser will create a user and assign it's basic groups
+// Note: It is advised that this function is used when creating users rather than directly calling j.Users().New()
+func (j *Jump) CreateUser(email, password string, groups ...string) (userID, apiKey string, err error) {
+	if userID, err = j.usrs.New(email, password); err != nil {
+		return
+	}
+
+	// Ensure first group is the user group
+	groups = append([]string{userID}, groups...)
+
+	// Add groups to user
+	if err = j.perm.AddGroup(userID, groups...); err != nil {
+		return
+	}
+
+	if apiKey, err = j.api.New(userID, "primary"); err != nil {
+		return
+	}
+
+	if err = j.SetPermission("user", userID, userID, permRWD, permRWD); err != nil {
+		return
+	}
+
+	return
+}
