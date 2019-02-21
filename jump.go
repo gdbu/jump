25bab634cee99faf2b516f553f515aee499eaa15
Josh Montoya <josh@montoya.io>
2019-02-20 17:13:18 -0800

    Move NewSession to sessions.go

--- jump.go
+++ jump.go
@@ -147,8 +147,3 @@ func (j *Jump) APIKeys() *apikeys.APIKeys {
 func (j *Jump) Permissions() *permissions.Permissions {
 	return j.perm
 }
-
-// NewSession will generate a new session for a given user ID
-func (j *Jump) NewSession(userID string) (key, token string, err error) {
-	return j.sess.New(userID)
-}
---
+++ sessions.go
@@ -0,0 +1,6 @@
+package jump
+
+// NewSession will generate a new session for a given user ID
+func (j *Jump) NewSession(userID string) (key, token string, err error) {
+	return j.sess.New(userID)
+}
