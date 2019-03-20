f6ec4ab314399b646f30f38312a52140c85e99f7
Josh Montoya <josh@montoya.io>
2019-03-20 15:31:37 -0700

    Implement GetAPIKeysByUser

---
+++ apikey.go
@@ -0,0 +1,8 @@
+package jump
+
+import "github.com/Hatch1fy/jump/apikeys"
+
+// GetAPIKeysByUser will return the api keys for a user
+func (j *Jump) GetAPIKeysByUser(userID string) (as []*apikeys.APIKey, err error) {
+	return j.api.GetByUser(userID)
+}
---
+++ plugin/apikey.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"github.com/Hatch1fy/httpserve"
+	"github.com/Hatch1fy/jump/apikeys"
+)
+
+// GetAPIKeysByUser is the handler for retrieving the api keys for a user
+func GetAPIKeysByUser(ctx *httpserve.Context) (res httpserve.Response) {
+	var (
+		as  []*apikeys.APIKey
+		err error
+	)
+
+	if as, err = p.jump.GetAPIKeysByUser(ctx.Param("userID")); err != nil {
+		return httpserve.NewJSONResponse(400, err)
+	}
+
+	return httpserve.NewJSONResponse(200, as)
+}
